c80cdd6cab3e68973c14e40298128df69667bb5a
Jack Lin <jacklin213@gmail.com>
2022-10-08 14:00:56 +0000

    Update config to always be latest instead of static

--- src/pages/Config.tsx
+++ src/pages/Config.tsx
@@ -1,9 +1,58 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AdRow } from "../components/Ads";
 import useDocumentTitle from "../utils/useDocumentTitle";
 
+const YamlCode = (props: any) => {
+  const { children } = props;
+  return (
+    <pre>
+      <code>{children}</code>
+    </pre>
+  );
+};
+
 export const Config = () => {
   useDocumentTitle("Config");
+  const [config, setConfig] = useState("");
+  const [cooldownConfig, setCooldownConfig] = useState("");
+  const [spellsConfig, setSpellsConfig] = useState("");
+
+  useEffect(() => {
+    if (!config) {
+      window
+        .fetch("https://raw.githubusercontent.com/HarryPotterSpells/HarryPotterSpells/master/src/config.yml")
+        .then((response) => response.text())
+        .then((data) => setConfig(data))
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [config]);
+
+  useEffect(() => {
+    if (!cooldownConfig) {
+      window
+        .fetch("https://raw.githubusercontent.com/HarryPotterSpells/HarryPotterSpells/master/src/cooldown.yml")
+        .then((response) => response.text())
+        .then((data) => setCooldownConfig(data))
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [cooldownConfig]);
+
+  useEffect(() => {
+    if (!spellsConfig) {
+      window
+        .fetch("https://raw.githubusercontent.com/HarryPotterSpells/HarryPotterSpells/master/src/spells.yml")
+        .then((response) => response.text())
+        .then((data) => setSpellsConfig(data))
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [spellsConfig]);
 
   return (
     <>
@@ -17,7 +66,14 @@ export const Config = () => {
       <h3 id="config">config.yml</h3>
       <div className="mt-4 mb-4">
         <div id="config-container">
-          <script src="https://pastebin.com/embed_js.php?i=Kn7r0GX5"></script>
+          {/*
+          // Can render embed with iFrame but styling is a pain
+          <iframe
+            title="config"
+            srcDoc={'<script src="https://pastebin.com/embed_js.php?i=Kn7r0GX5"></script>'}
+            style={{ borderWidth: 0 }}
+          ></iframe> */}
+          <YamlCode>{config}</YamlCode>
         </div>
       </div>
 
@@ -26,7 +82,7 @@ export const Config = () => {
       <h3 id="cooldown">cooldown.yml</h3>
       <div className="mt-4 mb-4">
         <div id="cooldown-container">
-          <script src="https://pastebin.com/embed_js.php?i=fe3J3Ysm"></script>
+          <YamlCode>{cooldownConfig}</YamlCode>
         </div>
       </div>
 
@@ -35,7 +91,7 @@ export const Config = () => {
       <h3 id="spells">spells.yml</h3>
       <div className="mt-4 mb-4">
         <div id="spells-container">
-          <script src="https://pastebin.com/embed_js.php?i=wSBTCJnQ"></script>
+          <YamlCode>{spellsConfig}</YamlCode>
         </div>
       </div>
 
